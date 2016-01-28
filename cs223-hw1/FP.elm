e42adb94c90f92fc7cccf9199848760036672e33
Prat <banqfirn@gmail.com>
2016-01-27 22:18:38 -0500

    Add hw1 FP

--- cs223-hw1/FP.elm
+++ cs223-hw1/FP.elm
@@ -1,8 +1,10 @@
+-- https://www.classes.cs.uchicago.edu/archive/2015/winter/22300-1/assignments/HW1.html
+-- http://package.elm-lang.org/packages/elm-lang/core/1.0.0/Result
 module FP where
 
-import List ((::))  -- TODO: modify imports if you'd like
+import List exposing ((::), sum, concatMap, filter, isEmpty, foldr, map, length)
 import List
-import Result (..)
+import Result
 
 digitsOfInt : Int -> List Int
 digitsOfInt n =
@@ -15,20 +17,32 @@ digitsOfInt n =
 
 additivePersistence : Int -> Int
 additivePersistence n =
-  -- TODO
-  0
+  if n < 10 then 0
+  else 1 + additivePersistence (sum(digitsOfInt n))
 
 digitalRoot : Int -> Int
 digitalRoot n =
-  -- TODO
-  0
+  if n < 10 then n
+  else digitalRoot(sum(digitsOfInt n))
+
+inits : List a -> List (List a)
+inits = foldr (\e acc -> []::map ((::)e) acc) [[]]
+
+tails : List a -> List (List a)
+tails = foldr tailsHelp [[]]
+
+tailsHelp : a -> List (List a) -> List (List a)
+tailsHelp e list =
+  case list of
+    (x::xs) ->
+      (e::x)::x::xs
+    [] ->
+      []
 
 subsequences : List a -> List (List a)
-subsequences xs =
-  -- TODO
-  []
+subsequences = (tails >> concatMap inits) >> filter (isEmpty >> not)
 
 take : Int -> List a -> Result String (List a)
 take k xs =
-  -- TODO
-  Err "..."
+  if k <= length xs then Ok (List.take k xs)
+  else Err "Yours is too short."
