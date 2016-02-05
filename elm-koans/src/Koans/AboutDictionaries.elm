6dba2742cfbf29e3a6aeb09a9f03d98f7878f3e5
Prat <banqfirn@gmail.com>
2016-02-04 20:09:15 -0500

    Finish About Dictionaries

--- elm-koans/src/Koans/AboutDictionaries.elm
+++ elm-koans/src/Koans/AboutDictionaries.elm
@@ -10,63 +10,63 @@ testSuite =
     "About Dictionaries"
     <| [ test
           "dictionaries can be empty"
-          (assertEqual xNum (Dict.size Dict.empty))
+          (assertEqual 0 (Dict.size Dict.empty))
        , test
           "or initialized with a single key-value pair"
-          (assertEqual xNum (Dict.size <| Dict.singleton 1 "a"))
+          (assertEqual 1 (Dict.size <| Dict.singleton 1 "a"))
        , test
           "or from a list of key-value pairs"
-          (assertEqual xNum (Dict.size <| Dict.fromList [ ( 1, "a" ), ( 2, "b" ) ]))
+          (assertEqual 2 (Dict.size <| Dict.fromList [ ( 1, "a" ), ( 2, "b" ) ]))
        , test
           "they can also be converted back to a list"
-          (assertEqual xList (Dict.toList <| Dict.fromList [ ( 1, "a" ), ( 2, "b" ) ]))
+          (assertEqual [ ( 1, "a" ), ( 2, "b" ) ] (Dict.toList <| Dict.fromList [ ( 1, "a" ), ( 2, "b" ) ]))
        , test
           "checking for an empty dictionary is easy"
-          (assertEqual xBool (Dict.isEmpty Dict.empty))
+          (assertEqual True (Dict.isEmpty Dict.empty))
        , test
           "you can also check if a key is present in the dictionary"
-          (assertEqual xBool (Dict.member 1 <| Dict.singleton 1 "a"))
+          (assertEqual True (Dict.member 1 <| Dict.singleton 1 "a"))
        , test
           "or get the value associated with the key"
-          (assertEqual (Just xString) (Dict.get 1 <| Dict.singleton 1 "a"))
+          (assertEqual (Just "a") (Dict.get 1 <| Dict.singleton 1 "a"))
        , test
           "a key-value pair can be added to the dictionary"
-          (assertEqual (Just xString) (Dict.get 2 <| Dict.insert 2 "b" <| Dict.singleton 1 "a"))
+          (assertEqual (Just "b") (Dict.get 2 <| Dict.insert 2 "b" <| Dict.singleton 1 "a"))
        , test
           "inserting can also overwrite the value associated with a key"
-          (assertEqual (Just xString) (Dict.get 1 <| Dict.insert 1 "b" <| Dict.singleton 1 "a"))
+          (assertEqual (Just "b") (Dict.get 1 <| Dict.insert 1 "b" <| Dict.singleton 1 "a"))
        , test
           "updating a value works similarly"
-          (assertEqual (Just xString) (Dict.get 1 <| Dict.update 1 (\_ -> Just "b") <| Dict.singleton 1 "a"))
+          (assertEqual (Just "b") (Dict.get 1 <| Dict.update 1 (\_ -> Just "b") <| Dict.singleton 1 "a"))
        , test
           "but knows about the current value"
-          (assertEqual (Just xString) (Dict.get 1 <| Dict.update 1 (\v -> (Maybe.withDefault "" v) ++ "b" |> Just) <| Dict.singleton 1 "a"))
+          (assertEqual (Just "ab") (Dict.get 1 <| Dict.update 1 (\v -> (Maybe.withDefault "" v) ++ "b" |> Just) <| Dict.singleton 1 "a"))
        , test
           "updating can add a new value"
-          (assertEqual (Just xString) (Dict.get 2 <| Dict.update 2 (\_ -> Just "b") <| Dict.singleton 1 "a"))
+          (assertEqual (Just "b") (Dict.get 2 <| Dict.update 2 (\_ -> Just "b") <| Dict.singleton 1 "a"))
        , test
           "or even remove a value"
-          (assertEqual (Just xString) (Dict.get 1 <| Dict.update 1 (\_ -> Nothing) <| Dict.singleton 1 "a"))
+          (assertEqual Nothing (Dict.get 1 <| Dict.update 1 (\_ -> Nothing) <| Dict.singleton 1 "a"))
        , test
           "of course there is a more direct way to remove a value too"
-          (assertEqual (Just xString) (Dict.get 1 <| Dict.remove 1 <| Dict.singleton 1 "a"))
+          (assertEqual Nothing (Dict.get 1 <| Dict.remove 1 <| Dict.singleton 1 "a"))
        , test
           "you can get the list of keys"
-          (assertEqual xList (Dict.keys <| Dict.singleton 1 "a"))
+          (assertEqual [ 1 ] (Dict.keys <| Dict.singleton 1 "a"))
        , test
           "or the list of values"
-          (assertEqual xList (Dict.values <| Dict.singleton 1 "a"))
+          (assertEqual [ "a" ] (Dict.values <| Dict.singleton 1 "a"))
        , test
           "you can get the union of two dictionaries"
-          (assertEqual xList (Dict.values <| Dict.union (Dict.fromList [ ( 1, "a" ), ( 2, "b" ) ]) (Dict.fromList [ ( 2, "c" ), ( 3, "d" ) ])))
+          (assertEqual [ "a", "b", "d" ] (Dict.values <| Dict.union (Dict.fromList [ ( 1, "a" ), ( 2, "b" ) ]) (Dict.fromList [ ( 2, "c" ), ( 3, "d" ) ])))
        , test
           "or the intersection"
-          (assertEqual xList (Dict.values <| Dict.intersect (Dict.fromList [ ( 1, "a" ), ( 2, "b" ) ]) (Dict.fromList [ ( 2, "c" ), ( 3, "d" ) ])))
+          (assertEqual [ "b" ] (Dict.values <| Dict.intersect (Dict.fromList [ ( 1, "a" ), ( 2, "b" ) ]) (Dict.fromList [ ( 2, "c" ), ( 3, "d" ) ])))
        , test
           "or the difference"
-          (assertEqual xList (Dict.values <| Dict.diff (Dict.fromList [ ( 1, "a" ), ( 2, "b" ) ]) (Dict.fromList [ ( 2, "c" ), ( 3, "d" ) ])))
+          (assertEqual [ "a" ] (Dict.values <| Dict.diff (Dict.fromList [ ( 1, "a" ), ( 2, "b" ) ]) (Dict.fromList [ ( 2, "c" ), ( 3, "d" ) ])))
        , test
           "mapping works similarly as with lists"
           -- so does filter, foldl, foldr, partition
-          (assertEqual xList (Dict.values <| Dict.map (\k v -> v ++ "c") (Dict.fromList [ ( 1, "a" ), ( 2, "b" ) ])))
+          (assertEqual [ "ac", "bc" ] (Dict.values <| Dict.map (\k v -> v ++ "c") (Dict.fromList [ ( 1, "a" ), ( 2, "b" ) ])))
        ]
