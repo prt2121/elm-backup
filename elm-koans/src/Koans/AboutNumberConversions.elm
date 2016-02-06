c78f3e9ed719b41e154daef91986c3b6ed2886ee
Prat <banqfirn@gmail.com>
2016-02-05 21:17:04 -0500

    Add About Number Conversions koans

--- elm-koans/src/Koans/AboutNumberConversions.elm
+++ elm-koans/src/Koans/AboutNumberConversions.elm
@@ -9,29 +9,29 @@ testSuite =
     "About Number Conversions"
     [ test
         "toFloat converts an int to a float"
-        (assertEqual xNum (toFloat 5))
+        (assertEqual 5.0 (toFloat 5))
     , test
         "floor converts a float to an int (rounding down)"
-        (assertEqual xNum (floor 3.6))
+        (assertEqual 3 (floor 3.6))
     , test
         "floor converts a float to an int (rounding down)"
-        (assertEqual xNum (floor -3.6))
+        (assertEqual -4 (floor -3.6))
     , test
         "ceiling converts a float to an int (rounding up)"
-        (assertEqual xNum (ceiling 3.6))
+        (assertEqual 4 (ceiling 3.6))
     , test
         "ceiling converts a float to an int (rounding up)"
-        (assertEqual xNum (ceiling -3.6))
+        (assertEqual -3 (ceiling -3.6))
     , test
         "round converts a float to an int (rounding to the closest int)"
-        (assertEqual xNum (round 3.6))
+        (assertEqual 4 (round 3.6))
     , test
         "round converts a float to an int (rounding to the closest int)"
-        (assertEqual xNum (round -3.6))
+        (assertEqual -4 (round -3.6))
     , test
         "truncate converts a float to an int (rounding towards 0)"
-        (assertEqual xNum (truncate 3.6))
+        (assertEqual 3 (truncate 3.6))
     , test
         "truncate converts a float to an int (rounding towards 0)"
-        (assertEqual xNum (truncate -3.6))
+        (assertEqual -3 (truncate -3.6))
     ]
