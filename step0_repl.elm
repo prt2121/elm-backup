448d91e92e4c70787126bea9a554f9332dde99aa
Prat <banqfirn@gmail.com>
2016-02-20 14:44:58 -0500

    Refactor and format step0

--- step0_repl.elm
+++ step0_repl.elm
@@ -1,38 +1,57 @@
-module Main where
+module Main (..) where
 
 import Console exposing (IO, (>>>), (>>=), forever, getLine, pure, exit, putStr, putStrLn)
 import Task
 
+
 -- read
+
+
 mal_read : String -> String
-mal_read str = str
+mal_read str =
+  str
+
+
 
 -- eval
+
+
 eval : String -> String -> String
-eval ast env = ast
+eval ast env =
+  ast
+
+
 
 -- print
+
+
 mal_print : String -> String
-mal_print exp = exp
+mal_print exp =
+  exp
+
+
 
 -- repl
+
+
 rep : String -> String
-rep line = mal_print <| eval (mal_read line) ""
+rep line =
+  eval (mal_read line) ""
+    |> mal_print
 
-echo : IO ()
-echo = forever (getLine >>= putStrLn)
-
-loop : IO ()
-loop = putStr "user> " >>>
-       getLine >>= \s ->
-         if s == ""
-         then pure ()
-         else (putStrLn <| rep s) >>> loop
-         -- else putStrLn s >>> loop
 
 repl_loop : IO ()
-repl_loop = loop >>>
-            exit 0
+repl_loop =
+  putStr "user> "
+    >>> getLine
+    >>= \s ->
+          if s == "" then
+            pure ()
+          else
+            (rep s |> putStrLn) >>> repl_loop
+
 
 port runner : Signal (Task.Task x ())
-port runner = Console.run repl_loop 
+port runner =
+  repl_loop >>> exit 0
+    |> Console.run
